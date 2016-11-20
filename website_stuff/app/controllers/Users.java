dd69e16c3cfc6e3be6af63e0957442dcefd5bc06
teddybri <tbrinso12@gmail.com>
2016-11-20 15:22:45 -0500

    Update Users.java

--- website_stuff/app/controllers/Users.java
+++ website_stuff/app/controllers/Users.java
@@ -18,7 +18,7 @@ public class Users {
 		int i;
 		for(i =0; i<100 && mName[i] != null; i++){
 			if(mName[i] == name){
-				flipID();
+				//flipID();
 				return;
 			}
 		}
@@ -26,17 +26,54 @@ public class Users {
 		envArray[i] = false;
 	}
 	
-	public static boolean envFind(){
-		int i;
+	public static int getIndex(){
+		nt i;
 		Security.Authenticator mSA = new Security.Authenticator();
-    	String name = mSA.getUsername(Http.Context.current());
+    		String name = mSA.getUsername(Http.Context.current());
 		for(i =0; i<100 && mName[i] != name; i++);
+		if(i>=100)
+			return-1;
+		else
+			return i;
+	}
+	
+	public static boolean getEnv(){
+		int i = getIndex();
 		boolean ret = envArray[i];
-		flipID();
+		//flipID();
 		return ret;
 	}
 	
-	public static void flipID(){
+	public static void setEnv(boolean env){
+		int i = getIndex();
+		envArray[i] = env;	
+	}
+	
+	public static String getPass(){
+		int i = getIndex();
+		String ret = mPass[i];
+		//flipID();
+		return ret;
+	}
+	
+	public static void setPass(String pass){
+		int i = getIndex();
+		mPass[i] = pass;	
+	}
+	
+	public static string getActName(){
+		int i = getIndex();
+		String ret = mActName[i];
+		//flipID();
+		return ret;
+	}
+	
+	public static void setActName(String ActName){
+		int i = getIndex();
+		mActName[i] = ActName
+	}
+	
+	/*public static void flipID(){
 		int i;
 		Security.Authenticator mSA = new Security.Authenticator();
     	String name = mSA.getUsername(Http.Context.current());
